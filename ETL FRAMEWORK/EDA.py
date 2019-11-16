45ea8995e9c540cc80493cac94652cddeab2561b
Sudipt-panda <44495830+Sudipt-panda@users.noreply.github.com>
2019-11-16 13:12:26 +0100

    Update EDA.py
    added comments

--- ETL FRAMEWORK/EDA.py
+++ ETL FRAMEWORK/EDA.py
@@ -15,19 +15,23 @@ logger3 = logging.getLogger(__name__)
 def EDA(df_preprocessed):
     logger3.info("EDA started")
     data = df_preprocessed
+    #Co-relation between gender and purchase
     cols = ['User_ID','Product_ID']
     data.drop(cols, inplace = True, axis =1)
     data[['Gender','Purchase']].groupby('Gender').mean().plot.bar()
     sns.barplot('Gender', 'Purchase', data = data)
     plt.show()
+    #Co-relation between Age and purchase
     data[['Age','Purchase']].groupby('Age').mean().plot.bar()
     sns.barplot('Age', 'Purchase', data = data)
     plt.show()
     sns.boxplot('Age','Purchase', data = data)
     plt.show()
+    #Co-relation between city_category and purchase
     data[['City_Category','Purchase']].groupby('City_Category').mean().plot.bar()
     sns.barplot('City_Category', 'Purchase', data = data)
     plt.show()
+    #heat-map to show co-relation between all variables
     corrmat = data.corr()
     fig,ax = plt.subplots(figsize = (12,9))
     sns.heatmap(corrmat, vmax=.8, square=True)
